f174f0adaccd89a744114b5edc15575a1ac61ba6
uvchik <uvchik.git@posteo.eu>
2021-06-25 14:55:37 +0200

    Use pytest

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -13,8 +13,6 @@ from itertools import chain
 from pprint import pformat
 
 import pandas as pd
-from nose.tools import eq_
-from nose.tools import ok_
 
 from oemof.network import energy_system as es
 from oemof.network.groupings import Flows
@@ -39,25 +37,25 @@ class TestsEnergySystem:
 
     def test_entity_registration(self):
         bus = Bus(label="bus-uid", type="bus-type")
-        eq_(self.es.nodes[0], bus)
+        assert self.es.nodes[0] == bus
         bus2 = Bus(label="bus-uid2", type="bus-type")
-        eq_(self.es.nodes[1], bus2)
+        assert self.es.nodes[1] == bus2
         t1 = Transformer(label="pp_gas", inputs=[bus], outputs=[bus2])
-        ok_(t1 in self.es.nodes)
+        assert t1 in self.es.nodes
         self.es.timeindex = self.timeindex
-        ok_(len(self.es.timeindex) == 5)
+        assert len(self.es.timeindex) == 5
 
     def test_entity_grouping_on_construction(self):
         bus = Bus(label="test bus")
         ensys = es.EnergySystem(entities=[bus])
-        ok_(ensys.groups[bus.label] is bus)
+        assert ensys.groups[bus.label] is bus
 
     def test_that_nodes_is_a_proper_alias_for_entities(self):
         b1, b2 = Bus(label="B1"), Bus(label="B2")
-        eq_(self.es.nodes, [b1, b2])
+        assert self.es.nodes == [b1, b2]
         empty = []
         self.es.nodes = empty
-        ok_(self.es.entities is empty)
+        assert self.es.entities is empty
 
     def test_that_none_is_not_a_valid_group(self):
         def by_uid(n):
@@ -72,14 +70,14 @@ class TestsEnergySystem:
             Entity(uid="Not in 'Group': {}".format(i)) for i in range(10)
         ]
         grouped = [Entity(uid="In 'Group': {}".format(i)) for i in range(10)]
-        ok_(None not in ensys.groups)
+        assert None not in ensys.groups
         for g in ensys.groups.values():
             for e in ungrouped:
                 if isinstance(g, Iterable) and not isinstance(g, str):
-                    ok_(e not in g)
+                    assert e not in g
             for e in grouped:
                 if isinstance(g, Iterable) and not isinstance(g, str):
-                    ok_(e in g)
+                    assert e in g
 
     @temporarily_modifies_registry
     def test_defining_multiple_groupings_with_one_function(self):
@@ -99,19 +97,15 @@ class TestsEnergySystem:
             for i in range(10)
         ]
         for group in ["Foo", "Bar", "A", "B"]:
-            eq_(
+            assert len(ensy.groups[group]) == 5, (
+                "\n  Failed testing length of group '{}'."
+                + "\n  Expected: 5"
+                + "\n  Got     : {}"
+                + "\n  Group   : {}"
+            ).format(
+                group,
                 len(ensy.groups[group]),
-                5,
-                (
-                    "\n  Failed testing length of group '{}'."
-                    + "\n  Expected: 5"
-                    + "\n  Got     : {}"
-                    + "\n  Group   : {}"
-                ).format(
-                    group,
-                    len(ensy.groups[group]),
-                    sorted([e.label for e in ensy.groups[group]]),
-                ),
+                sorted([e.label for e in ensy.groups[group]]),
             )
 
     def test_grouping_filter_parameter(self):
@@ -128,8 +122,8 @@ class TestsEnergySystem:
         others = set(Node(label="other: {}".format(i)) for i in range(10))
         ensys.add(special, *subset)
         ensys.add(*others)
-        eq_(ensys.groups["The Special One"], special)
-        eq_(ensys.groups["A Subset"], subset)
+        assert ensys.groups["The Special One"] == special
+        assert ensys.groups["A Subset"] == subset
 
     def test_proper_filtering(self):
         """`Grouping.filter` should not be "all or nothing".
@@ -147,7 +141,7 @@ class TestsEnergySystem:
         ensys = es.EnergySystem(groupings=[g])
         special = Node(label="object")
         ensys.add(special)
-        eq_(ensys.groups["group"], {2, 4})
+        assert ensys.groups["group"] == {2, 4}
 
     def test_non_callable_group_keys(self):
         collect_everything = Nodes(key="everything")
@@ -162,9 +156,9 @@ class TestsEnergySystem:
         everything = subset.union(others)
         everything.add(special)
         ensys.add(*everything)
-        eq_(ensys.groups["The Special One"], special)
-        eq_(ensys.groups["A Subset"], subset)
-        eq_(ensys.groups["everything"], everything)
+        assert ensys.groups["The Special One"] == special
+        assert ensys.groups["A Subset"] == subset
+        assert ensys.groups["everything"] == everything
 
     def test_grouping_laziness(self):
         """Energy system `groups` should be fully lazy.
@@ -180,8 +174,7 @@ class TestsEnergySystem:
         self.es.add(buses[0])
         buses[0].group = True
         self.es.add(*buses[1:])
-        ok_(
-            group in self.es.groups,
+        assert group in self.es.groups, (
             (
                 "\nExpected to find\n\n  `{!r}`\n\n"
                 "in `es.groups`.\nGot:\n\n  `{}`"
@@ -190,8 +183,7 @@ class TestsEnergySystem:
                 "\n   ".join(pformat(set(self.es.groups.keys())).split("\n")),
             ),
         )
-        ok_(
-            buses[0] in self.es.groups[group],
+        assert buses[0] in self.es.groups[group], (
             "\nExpected\n\n  `{}`\n\nin `es.groups['{}']`:\n\n  `{}`".format(
                 "\n   ".join(pformat(buses[0]).split("\n")),
                 group,
@@ -215,9 +207,10 @@ class TestsEnergySystem:
         ensys = es.EnergySystem(groupings=[collect_everything])
         Node.registry = ensys
         node = Node(label="A Node")
-        ok_("everything" not in ensys.groups)
-        ok_(everything in ensys.groups)
-        eq_(ensys.groups[everything], {node})
+        assert "everything" not in ensys.groups
+        assert everything in ensys.groups
+        assert ensys.groups[everything] == {node}
+        assert everything() == "everything"
 
     @temporarily_modifies_registry
     def test_flows(self):
@@ -226,9 +219,8 @@ class TestsEnergySystem:
         Node.registry = ensys
         bus = Bus(label="A Bus")
         Node(label="A Node", inputs={bus: None}, outputs={bus: None})
-        eq_(
-            ensys.groups[key],
-            set(chain(bus.inputs.values(), bus.outputs.values())),
+        assert ensys.groups[key] == set(
+            chain(bus.inputs.values(), bus.outputs.values())
         )
 
     @temporarily_modifies_registry
@@ -238,10 +230,10 @@ class TestsEnergySystem:
         Node.registry = ensys
         bus = Bus(label="A Bus")
         node = Node(label="A Node", inputs={bus: None}, outputs={bus: None})
-        eq_(
-            ensys.groups[key],
-            {(bus, node, bus.outputs[node]), (node, bus, node.outputs[bus])},
-        )
+        assert ensys.groups[key], {
+            (bus, node, bus.outputs[node]),
+            (node, bus, node.outputs[bus]),
+        }
 
     def test_that_node_additions_are_signalled(self):
         """
@@ -250,8 +242,8 @@ class TestsEnergySystem:
         node = Node(label="Node")
 
         def subscriber(sender, **kwargs):
-            ok_(sender is node)
-            ok_(kwargs["EnergySystem"] is self.es)
+            assert sender is node
+            assert kwargs["EnergySystem"] is self.es
             subscriber.called = True
 
         subscriber.called = False
@@ -260,11 +252,8 @@ class TestsEnergySystem:
             subscriber, sender=node
         )
         self.es.add(node)
-        ok_(
-            subscriber.called,
-            (
-                "\nExpected `subscriber.called` to be `True`.\n"
-                "Got {}.\n"
-                "Probable reason: `subscriber` didn't get called."
-            ).format(subscriber.called),
-        )
+        assert subscriber.called, (
+            "\nExpected `subscriber.called` to be `True`.\n"
+            "Got {}.\n"
+            "Probable reason: `subscriber` didn't get called."
+        ).format(subscriber.called)
