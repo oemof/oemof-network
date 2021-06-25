2b233d7200517bf8dba103ce0cbcd40dab3bb957
uvchik <uvchik.git@posteo.eu>
2021-06-25 09:40:52 +0200

    Add python version 3.9 to tests and drop 3.6

--- setup.py
+++ setup.py
@@ -57,9 +57,9 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         # uncomment if you test on these interpreters:
         # 'Programming Language :: Python :: Implementation :: IronPython',
@@ -77,7 +77,7 @@ setup(
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=["pandas", "blinker", "dill", "networkx"],
     extras_require={
         # eg:
