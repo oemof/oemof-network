b7da1b1afc21ec24334a4d450878ca606f4c30c1
Patrik Schönfeldt <patrik.schoenfeldt@dlr.de>
2022-03-24 21:01:24 +0100

    Define API

--- src/oemof/network/__init__.py
+++ src/oemof/network/__init__.py
@@ -1,1 +1,25 @@
 __version__ = "0.4.0rc0"
+
+import energy_system
+import graph
+import groupings
+import network
+from .network import (
+    Bus,
+    Component,
+    Sink,
+    Source,
+    Transformer,
+)
+
+__all__ = [
+    "Bus",
+    "Component",
+    "energy_system",
+    "graph",
+    "groupings",
+    "network",
+    "Sink",
+    "Source",
+    "Transformer",
+]
