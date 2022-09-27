5409645de6f9af2ed767036756d77b56b21b258e
Patrik Schönfeldt <patrik@salacia.snft.de>
2022-09-27 16:20:36 +0200

    Create v0.5 alpha release

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ long_description = "%s" % (
 
 setup(
     name="oemof.network",
-    version="0.4.0",
+    version="0.5.0.alpha1",
     license="MIT",
     description="The network/graph submodules of oemof.",
     long_description=long_description,
