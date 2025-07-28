7f131451f47b01d38ac67688766dfc83c266f895
Patrik Schönfeldt <patrik@salacia.snft.de>
2025-07-28 12:50:43 +0200

    Improve SubNetwork  documentation

--- src/oemof/network/network/hierachical_nodes.py
+++ src/oemof/network/network/hierachical_nodes.py
@@ -121,21 +121,51 @@ class SubNetwork(Node):
         Create a subnode by calling `class_(label, *args, **kwargs)` and
         `append` the result to `self.subnodes`.
         The purpose of this wrapper is to make sure that subnodes are
-        always `label`led with a `SubNetworkLabel`. While not as
-        convenient as simply instantiating nodes and adding them to a
-        `SubNetwork` yourself, using this is as easy as replacing e.g.
-        the sequence
+        always `label`led with a `SubNetworkLabel`.
+        This is useful to ensure that the `label` of the subnode is unique
+        within the `SubNetwork` and that it can be identified as a subnode of
+        the `SubNetwork`.
 
-        ..code:: python
-            bus = Bus("bus", inputs={input: x}, outputs={output: y})
-            subnetwork.subnodes.append(bus)
+        Parameters
+        ----------
+        class_: type
+            The class of the subnode to create. This class must be a subclass
+            of `Node`.
+        label: hashable
+            The label to use for the subnode.
+        *args, **kwargs:
+            Additional positional and keyword arguments that will be passed to
+            the constructor of `class_` when creating the subnode.
 
-        with
+        Returns
+        -------
+        :class:`Node`
+            The newly created subnode, which is also appended to
+            `self.subnodes`.
 
-        ..code:: python
-            subnetwork.subnode(
-                Bus, "bus", inputs={input: x}, outputs={output: y}
-            )
+
+        Examples
+        --------
+        Create a subnode of type `Bus` with the given `label`, `inputs` and
+        `outputs` and append it to the `subnodes` of this `SubNetwork`.
+
+        When
+        >>> from oemof.network import SubNetwork, Node, Edge
+        >>> subnetwork = SubNetwork("subnetwork")
+        >>> input = output = Node("input")
+        >>>
+        >>> # Create a subnode of type `Node` using this convenience function
+        >>> bus = subnetwork.subnode(
+        >>>     Node, "bus", inputs={input: Edge()}, outputs={output: Edge()}
+        >>> )
+        >>>
+        >>> # Alternatively, do the same manually
+        >>> bus2 = Node(
+        >>>     HierachicalLabel("bus2", parent=subnetwork),
+        >>>     inputs={input: Edge()},
+        >>>     outputs={output: Edge()},
+        >>> )
+        >>> subnetwork.subnodes.append(bus)
 
         """
         subnode = class_(
