a5005f77e078766e4d16c58b73d4240de4582b2a
Patrik Schönfeldt <Patrik.Schoenfeldt@dlr.de>
2025-07-28 08:32:42 +0200

    Fix import order

--- src/oemof/network/network/__init__.py
+++ src/oemof/network/network/__init__.py
@@ -1,13 +1,13 @@
 from .edge import Edge
 from .entity import Entity
+from .hierachical_nodes import AtomicNode
+from .hierachical_nodes import SubNetwork
 from .nodes import Bus
 from .nodes import Component
 from .nodes import Node
 from .nodes import Sink
 from .nodes import Source
 from .nodes import Transformer
-from .hierachical_nodes import AtomicNode
-from .hierachical_nodes import SubNetwork
 
 __all__ = [
     "AtomicNode",
