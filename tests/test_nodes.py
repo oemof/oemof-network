b7381694887739b59550cd7bb7abba7dcbff4d1d
Patrik Schönfeldt <github@patrikschoenfeldt.de>
2025-11-06 11:25:31 +0100

    Fix intent

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -174,8 +174,8 @@ class TestNode:
         assert node1 != node3
         assert node2 != node3
 
-node1a = Node("node")
-assert node1 == node1a
+        node1a = Node("node")
+        assert node1 == node1a
 
         label = "node"
         assert node1 == label
